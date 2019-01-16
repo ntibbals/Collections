c2b9b6e1631e3058bc14ac1d2d7887b9f7f4eb4f
ntibbals <ntibbals@gmail.com>
2019-01-16 10:06:37 -0800

    setup add method within container for deck of cards

--- Lab07-Collections/Lab07-Collections/Classes/Container.cs
+++ Lab07-Collections/Lab07-Collections/Classes/Container.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace Lab07_Collections.Classes
 {
-    public class Container
+    public class Container<T> 
     {
+
+        T[] cardDeck = new T[10];
+        int currentIndex = 0;
+
+        public void Add(T card)
+        {
+            if (currentIndex > cardDeck.Length -1)
+            {
+                Array.Resize(ref cardDeck, cardDeck.Length * 2);
+            }
+
+            cardDeck[currentIndex] = card;
+            currentIndex++;
+        }
     }
 }
