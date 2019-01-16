9abc588f030f4a16b2f560366bff7fc238d88f62
ntibbals <ntibbals@gmail.com>
2019-01-16 09:57:01 -0800

    setup environment

---
+++ Lab07-Collections/Lab07-Collections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab07_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
